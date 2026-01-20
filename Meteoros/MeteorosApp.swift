85490e4cc4a9786feff9e9e0bc09e7a4d14da1de
ludik <bakhanetssimon@gmail.com>
2026-01-20 17:49:08 +0300

    Initial Commit

---
+++ Meteoros/MeteorosApp.swift
@@ -0,0 +1,17 @@
+//
+//  MeteorosApp.swift
+//  Meteoros
+//
+//  Created by Simon Bakhanets on 20.01.2026.
+//
+
+import SwiftUI
+
+@main
+struct MeteorosApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ MeteorosTests/MeteorosTests.swift
@@ -0,0 +1,16 @@
+//
+//  MeteorosTests.swift
+//  MeteorosTests
+//
+//  Created by Simon Bakhanets on 20.01.2026.
+//
+
+import Testing
+
+struct MeteorosTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
